f75d6eb564c71761dae9335933b85b7510e1b6d0
meidad hoze <48644044+medad-hoze@users.noreply.github.com>
2021-11-24 15:46:23 +0200

    Add files via upload

--- mapbox/style.js
+++ mapbox/style.js
@@ -7,9 +7,19 @@ var styleJSON = {
         "intensity": 0.2
     },
     "sources": {
-        "Building_0": {
+        "OSMStandard_0": {
+            "type": "raster",
+            "tiles": ["http://tile.openstreetmap.org/{z}/{x}/{y}.png"],
+            "tileSize": 256
+        },
+        "BLDG_cut_1": {
             "type": "geojson",
-            "data": json_Building_0
+            "data": json_BLDG_cut_1
+        }
+                    ,
+        "Building_2": {
+            "type": "geojson",
+            "data": json_Building_2
         }
                     },
     "sprite": "",
@@ -24,11 +34,24 @@ var styleJSON = {
             }
         },
         {
-            "id": "lyr_Building_0_0",
-            "type": "fill",
-            "source": "Building_0",
+            "id": "lyr_OSMStandard_0_0",
+            "type": "raster",
+            "source": "OSMStandard_0"
+        },
+        {
+            "id": "lyr_BLDG_cut_1_0",
+            "type": "line",
+            "source": "BLDG_cut_1",
             "layout": {},
-            "paint": {'fill-opacity': 1.0, 'fill-color': '#85b66f'}
+            "paint": {'line-width': 6.285714285714286, 'line-opacity': 1.0, 'line-color': '#3579b1'}
+        }
+,
+        {
+            "id": "lyr_Building_2_0",
+            "type": "line",
+            "source": "Building_2",
+            "layout": {},
+            "paint": {'line-width': 4.142857142857142, 'line-opacity': 1.0, 'line-color': '#e41a1c'}
         }
 ],
 }
